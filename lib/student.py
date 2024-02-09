f45d03e40d14c12858643b4c3105e6c3a57b352b
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-02-09 09:42:08 -0500

    completed lab

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,11 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def __init__(self, last_name, first_name):
+       
+        super().__init__(last_name, first_name)
+        self.knowledge = []
+    
+    def learn(self, string):
+        self.knowledge.append(string)
+        
